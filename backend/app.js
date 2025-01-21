b82662d8c9c47f72c6e274fdd5524e54b9cda4cc
eyoaab <eyobtariku48@gmail.com>
2025-01-21 23:16:17 +0300

    created the app file and the server file

---
+++ backend/app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+const connectDB = require("./configuration/db.config");
+
+// Routes
+const userRoutes = require("./routes/user.routes");
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.use("/users", userRoutes);
+
+// Global error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(err.status || 500).json({
+    message: err.message || "Internal Server Error",
+    stack: process.env.NODE_ENV === "production" ? null : err.stack,
+  });
+});
+
+module.exports = app;
