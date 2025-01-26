8902ca03d61179210c65b4b5ec5007b4ccd70aef
eyoaab <eyobtariku48@gmail.com>
2025-01-27 00:30:06 +0300

    added autentication for the user in the  chat page

--- backend/routes/chat.routes.js
+++ backend/routes/chat.routes.js
@@ -2,10 +2,12 @@ const {
   getAmharicResponse,
   getEnglishResponse,
 } = require("../controllers/chat.controller");
+const authenticateToken = require("../middlewares/auth.middleware");
+
 const router = require("express").Router();
 
 // chat route
-router.post("/amharic", getAmharicResponse);
-router.post("/english", getEnglishResponse);
+router.post("/amharic", authenticateToken, getAmharicResponse);
+router.post("/english", authenticateToken, getEnglishResponse);
 
 module.exports = router;
