0df8175003f54a280a3c781f29391fa91d7432be
eyoaab <eyobtariku48@gmail.com>
2025-02-05 10:43:06 +0300

    added utlity functions

---
+++ frontend/ethio_gpt/lib/cors/utility-functions/hide-keybord.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/widgets.dart';
+
+void hideKeyboard(BuildContext context) {
+  FocusScopeNode currentFocus = FocusScope.of(context);
+
+  if (!currentFocus.hasPrimaryFocus) {
+    currentFocus.unfocus();
+  }
+}
